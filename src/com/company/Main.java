3ad240f0de571b6412f5e33486438d7b8a1b7686
iAnalysis <al.shiganov@yandex.ru>
2017-02-09 19:09:02 +0300

    test1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Simple test");
-        System.out.println("Work!!!");
+        System.out.println("Work...");
     }
 }
