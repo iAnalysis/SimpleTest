b7c417583680d91573f2a848f649ff8b1eca1580
alexshiganov <al.shiganov@yandex.ru>
2017-02-09 19:25:25 +0300

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Simple test");
         System.out.println("Work...");
+        System.out.println("Vatafak?");
     }
 }
